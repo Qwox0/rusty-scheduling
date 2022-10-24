ce3d7df5284ad612217589f733dcd833d25ae278
Qwox <qwox@qwox.com>
2022-10-24 21:09:40 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use task_scheduling::{scheduler::Scheduler, tasks::Tasks, task::Task};
+use task_scheduling::{
+    scheduler::{Scheduler, SchedulerCondition::*},
+    tasks::Tasks,
+};
 
 fn main() {
-    let tasks1 = Tasks::from(vec![(1,4),(2,5),(2,7)]);
-    let tasks2 = Tasks::from(vec![(1,3),(1,5),(2,7)]);
+    #[allow(unused)]
+    let tasks1 = Tasks::from(vec![(1, 4), (2, 5), (2, 7)]);
+    #[allow(unused)]
+    let tasks2 = Tasks::from(vec![(1, 3), (1, 5), (2, 7)]);
+    #[allow(unused)]
+    let tasks3 = Tasks::from(vec![(1, 3), (1, 4), (2, 5)]);
 
-    let compare_deadline = |active: &Task, next: &Task, step:usize| active.until_deadline(step) <= next.until_deadline(step);
     // Earliest Due Date
-    let mut edd = Scheduler::new(false, compare_deadline);
+    let mut edd = Scheduler::new(false, vec![MinDeadline, FCFS]);
     // Earliest Deadline First
-    let mut edf = Scheduler::new(true, compare_deadline);
+    let mut edf = Scheduler::new(true, vec![MinDeadline, FCFS]);
 
-    let tasks = tasks1;
+    let tasks = tasks3;
     println!("{}", tasks);
-    println!("Earliest Due Date:        {}", edd.get_scheduling(&tasks, None));
-    println!("Earliest Deadline First:  {}", edf.get_scheduling(&tasks, None));
+    println!(
+        "Earliest Due Date:        {}",
+        edd.get_scheduling(&tasks, None)
+    );
+    println!(
+        "Earliest Deadline First:  {}",
+        edf.get_scheduling(&tasks, None)
+    );
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,23 +1,15 @@
-use std::ops::{Add, Bound, Bound::*, RangeBounds};
-
 use crate::{task::Task, tasks::Tasks};
 
 /// currently only Earliest Deadline First (preemptive)
 /// and Earliest Due Date (non-preemptive; must finish task)
-pub struct Scheduler<F>
-where
-    F: Fn(&Task, &Task, usize) -> bool,
-{
+pub struct Scheduler {
     is_preemptive: bool,
-    replace_condition: F,
+    conditions: Vec<SchedulerCondition>,
     waiting_tasks: Vec<Task>,
     active_task: Option<Task>,
 }
 
-impl<F> Scheduler<F>
-where
-    F: Fn(&Task, &Task, usize) -> bool,
-{
+impl Scheduler {
     /// Returns a new scheduling algorithm
     ///
     /// # Arguments
@@ -25,10 +17,10 @@ where
     /// * `is_preemptive` - Whether the algorithm checks `replace_condition` every step
     /// * `replace_condition` - A clossure which takes the active Task and a new Task and returns
     /// wheather the new Task should be selected for as the active Task
-    pub fn new(is_preemptive: bool, replace_condition: F) -> Self {
+    pub fn new(is_preemptive: bool, conditions: Vec<SchedulerCondition>) -> Self {
         Self {
             is_preemptive,
-            replace_condition,
+            conditions,
             waiting_tasks: vec![],
             active_task: None,
         }
@@ -42,7 +34,7 @@ where
         };
 
         let mut str = "".to_string();
-        for step in 0..=max_step {
+        for step in 1..=max_step {
             self.refresh_tasks(tasks, step);
             self.find_active_task(step);
             if let Some(active_task) = &mut self.active_task {
@@ -63,49 +55,71 @@ where
 
     fn refresh_tasks(&mut self, tasks: &Tasks, step: usize) {
         tasks.iter().for_each(|t: &Task| {
-            if step % t.period == 0 {
+            // period == deadline. task isn't available again on the deadline (only the cycle after)
+            if step % t.period == 1 {
                 self.waiting_tasks.push(t.clone())
             }
         });
     }
 
     fn find_active_task(&mut self, step: usize) {
-        match self.active_task {
-            Some(_) if !self.is_preemptive => (),
-            _ => {
-                (self.active_task, self.waiting_tasks) = find_active_task(
-                    self.active_task.clone(),
-                    self.waiting_tasks.clone(),
-                    step,
-                    &self.replace_condition,
-                )
-            }
+        if self.active_task.is_some() && !self.is_preemptive {
+            return;
         }
+        self.waiting_tasks =
+            self.waiting_tasks
+                .clone()
+                .into_iter()
+                .fold(vec![], |mut waiting: Vec<Task>, task| {
+                    for condition in &self.conditions {
+                        match condition.do_replace(&self.active_task, &task, step) {
+                            ConditionsResult::Replace => {
+                                if let Some(active) = self.active_task.clone() {
+                                    waiting.push(active);
+                                }
+                                self.active_task = Some(task);
+                                return waiting;
+                            }
+                            ConditionsResult::Keep => {
+                                waiting.push(task);
+                                return waiting;
+                            }
+                            ConditionsResult::NextCondition => continue,
+                        }
+                    }
+                    // default: no replace (first come first serve)
+                    waiting.push(task);
+                    waiting
+                })
     }
 }
 
-fn find_active_task<F>(
-    active_task: Option<Task>,
-    waiting_tasks: Vec<Task>,
-    step: usize,
-    replace_condition: &F,
-) -> (Option<Task>, Vec<Task>)
-where
-    F: Fn(&Task, &Task, usize) -> bool,
-{
-    waiting_tasks.into_iter().fold(
-        (active_task, vec![]),
-        |(active, mut waiting): (Option<Task>, Vec<Task>), task: Task| match active {
-            //Some(active) if !replace_condition(&active, &task) => {
-            Some(a) if replace_condition(&a, &task, step) => {
-                waiting.push(task);
-                (Some(a), waiting)
+pub enum SchedulerCondition {
+    MinDeadline,
+    FCFS, // First Come First Serve
+}
+
+pub enum ConditionsResult {
+    Replace,
+    Keep,
+    NextCondition,
+}
+
+impl SchedulerCondition {
+    pub fn do_replace(&self, active: &Option<Task>, next: &Task, step: usize) -> ConditionsResult {
+        use ConditionsResult::*;
+        use SchedulerCondition::*;
+        if let Some(a) = active {
+            match self {
+                MinDeadline => match (a.until_deadline(step), next.until_deadline(step)) {
+                    (ad, nd) if ad == nd => NextCondition,
+                    (ad, nd) if ad > nd => Replace,
+                    (_, _) => Keep,
+                },
+                FCFS => Keep,
             }
-            Some(a) => {
-                waiting.push(a);
-                (Some(task), waiting)
-            }
-            None => (Some(task), waiting),
-        },
-    )
+        } else {
+            return Replace;
+        }
+    }
 }
